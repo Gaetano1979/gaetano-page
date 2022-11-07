eb52f6c3b304791aedc97b77d9ecd856865734ef
@Gaetano79 <gaetano.sabino@gmail.com>
2022-11-07 06:42:24 +0100

    secondo commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/index.css';
 import '../styles/globals.css';
 import {AppPropsWithLayout} from "./profilo";
+import {createContext} from "react";
+
+const provaContext: {nome: string, cognome: string} = {
+  nome: 'Gaetano',
+  cognome: 'Sabino'
+}
+
+export const TestContext = createContext(provaContext)
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
--- pages/profilo/index.tsx
+++ pages/profilo/index.tsx
@@ -1,16 +1,41 @@
-import {ReactElement, ReactNode} from "react";
+import {ReactElement, ReactNode, useContext, useEffect, useMemo, useState} from "react";
 import {NextPage, NextPageContext} from "next";
 import {AppProps} from "next/app";
 import {HeaderLayout} from "../../app/Layout";
 import colors from "tailwindcss/colors";
+import {TestContext} from "../_app";
 
 export default function Profilo({Component, pageProps}: AppPropsWithLayout) {
+    const [value, setValue] = useState('gaetano');
+
+    useEffect(() => {
+        // document.title = `Proviamo a vedere che cosa succede ${value}`;
+        console.log('proviamo questa e la prima volta che viene chiamato');
+    });
+    //
+    //
+    // useEffect(() => {
+    //     // const sub = pageProps.source.subscribe();
+    //     console.log(value,pageProps, Component);
+    //     // return () => sub.unsubscribe();
+    //
+    // },[value])
+    // const changeName = () => {
+    //     if (value === 'gaetano') {
+    //         setValue('giovanna')
+    //     }else{
+    //         setValue( 'gaetano');
+    //     }
+    // }
+
+    const useContext1 = useContext(TestContext);
     return (
         <>
             <HeaderLayout title={'Gaetano Sabino'}/>
             <main className="bg-amber-700 h-screen p-3">
                 <h1 className="text-6xl font-bold uppercase text-center border">
-                    Hello Word
+                    {/*{value}*/}
+                    {value}
                 </h1>
                 <blockquote className="border p-2 mt-3.5 rounded-md">
                     <article className="text-white uppercase">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
